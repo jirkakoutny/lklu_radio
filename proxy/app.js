7be32686a0a8a2f7588d6cadc788f21f10a37d19
Jiri Koutny <jiri.koutny@strv.com>
2023-05-30 01:09:57 +0200

    New try with proxy

---
+++ proxy/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const request = require('request');
+const xml2json =  require('@hendt/xml2json');
+const xml = require('xml')
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/data', (req, res) => {
+  request(
+    { url: 'http://meteo.aeroklubluhacovice.cz:8081/xml.xml' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      
+      res.set('Content-Type', 'text/xml');
+      res.send(xml(body));
+    }
+  )
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+
+// const express = require('express');
+  
+// const app = express();
+// const PORT = 3000;
+  
+// app.listen(PORT, (error) =>{
+//     if(!error)
+//         console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+//     else 
+//         console.log("Error occurred, server can't start", error);
+//     }
+// );
