181a4d89b6234485a3b0b375da0f42a7b676eb50
Jiri Koutny <jiri.koutny@strv.com>
2023-05-30 14:48:53 +0200

    Url in EV

--- lklu_radio/src/App.js
+++ lklu_radio/src/App.js
@@ -1,46 +1,65 @@
-import './App.css'
-import { useState, useEffect } from "react"
-import moment from 'moment'
-import axios from "axios"
-import xml2json from '@hendt/xml2json';
+import "./App.css";
+import { useState, useEffect } from "react";
+import moment from "moment";
+import axios from "axios";
+import xml2json from "@hendt/xml2json";
 
 function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-
   useEffect(() => {
     const getData = async () => {
       try {
-
-        
-        const response = await axios.get(
-          // `meteo.aeroklubluhacovice.cz:8081/xml.xml`
-          `https://lklu-meteo-proxy.vercel.app/data`
-        );
-
-
-
+        const response = await axios.get(process.env.fetch_url);
 
         const json = xml2json(response.data);
-        const datetime = moment(json.wario.date + " " + json.wario.time, "YYYY-M-D h:m:s")
+        const datetime = moment(
+          json.wario.date + " " + json.wario.time,
+          "YYYY-M-D h:m:s"
+        );
 
         const output = {
           datetime: datetime.format("DD-MM-YYYY hh:mm:ss"),
           rwy: 20,
-          temperature: json.wario.input.sensor.filter(x => x.type === 'temperature')[0]?.value + '°' + json.wario.degree,
-          dewPoint: json.wario.input.sensor.filter(x => x.type === 'dew_point')[0]?.value + '°' + json.wario.degree,
-          pressure: json.wario.input.sensor.filter(x => x.type === 'pressure')[0]?.value + json.wario.pressure,
-          windDirection: json.wario?.input?.sensor?.filter(x => x.type === 'wind_direction')[0]?.value + '°',
-          windSpeed: Math.round(json.wario?.input?.sensor?.filter(x => x.type === 'wind_speed')[0]?.value * 1.9438452) + "kt",
-          windGust: Math.round(json.wario?.input?.sensor?.filter(x => x.type === 'wind_gust')[0]?.value * 1.9438452) + "kt",
-          windDirectionRaw: json.wario?.input?.sensor?.filter(x => x.type === 'wind_direction')[0]?.value,
+          temperature:
+            json.wario.input.sensor.filter((x) => x.type === "temperature")[0]
+              ?.value +
+            "°" +
+            json.wario.degree,
+          dewPoint:
+            json.wario.input.sensor.filter((x) => x.type === "dew_point")[0]
+              ?.value +
+            "°" +
+            json.wario.degree,
+          pressure:
+            json.wario.input.sensor.filter((x) => x.type === "pressure")[0]
+              ?.value + json.wario.pressure,
+          windDirection:
+            json.wario?.input?.sensor?.filter(
+              (x) => x.type === "wind_direction"
+            )[0]?.value + "°",
+          windSpeed:
+            Math.round(
+              json.wario?.input?.sensor?.filter(
+                (x) => x.type === "wind_speed"
+              )[0]?.value * 1.9438452
+            ) + "kt",
+          windGust:
+            Math.round(
+              json.wario?.input?.sensor?.filter(
+                (x) => x.type === "wind_gust"
+              )[0]?.value * 1.9438452
+            ) + "kt",
+          windDirectionRaw: json.wario?.input?.sensor?.filter(
+            (x) => x.type === "wind_direction"
+          )[0]?.value,
           civilStart: json.wario?.variable?.civstart,
           civilEnd: json.wario?.variable?.civend,
-        }
+        };
 
-        setData(output)
+        setData(output);
         setError(null);
       } catch (err) {
         setError(err.message);
@@ -66,16 +85,40 @@ function App() {
       )}
 
       <div className="mockup-code">
-        <pre data-prefix="$">LKLU radio <code>125,285</code></pre>
-        <pre data-prefix=">" className="text-warning">Time: <code>{!loading && data.datetime}</code></pre>
-        <pre data-prefix=">" className="text-warning">Rwy: <code>{!loading && pad(data.rwy / 10, 2) + "/" + pad((data.rwy + 180) / 10, 2)}</code></pre>
-        <pre data-prefix=">" className="text-success">QNH: <code>{!loading && data.pressure}</code></pre>
-        <pre data-prefix=">" className="text-warning">Temp/Dew point: <code>{!loading && data.temperature + "/" + data.dewPoint}</code></pre>
-        <pre data-prefix=">" className="text-success">Wind: <code>{!loading && data.windDirection + "/" + data.windSpeed}</code></pre>
-        {(!loading && data.windSpeed !== data.windGust) && <pre data-prefix=">" className="text-success">Wind Gust: <code>{!loading && data.windGust}</code></pre>}
-        <pre data-prefix=">" className="text-success">Civil twilight: <code>{!loading && data.civilStart + "-" + data.civilEnd}</code></pre>
+        <pre data-prefix="$">
+          LKLU radio <code>125,285</code>
+        </pre>
+        <pre data-prefix=">" className="text-warning">
+          Time: <code>{!loading && data.datetime}</code>
+        </pre>
+        <pre data-prefix=">" className="text-warning">
+          Rwy:{" "}
+          <code>
+            {!loading &&
+              pad(data.rwy / 10, 2) + "/" + pad((data.rwy + 180) / 10, 2)}
+          </code>
+        </pre>
+        <pre data-prefix=">" className="text-success">
+          QNH: <code>{!loading && data.pressure}</code>
+        </pre>
+        <pre data-prefix=">" className="text-warning">
+          Temp/Dew point:{" "}
+          <code>{!loading && data.temperature + "/" + data.dewPoint}</code>
+        </pre>
+        <pre data-prefix=">" className="text-success">
+          Wind:{" "}
+          <code>{!loading && data.windDirection + "/" + data.windSpeed}</code>
+        </pre>
+        {!loading && data.windSpeed !== data.windGust && (
+          <pre data-prefix=">" className="text-success">
+            Wind Gust: <code>{!loading && data.windGust}</code>
+          </pre>
+        )}
+        <pre data-prefix=">" className="text-success">
+          Civil twilight:{" "}
+          <code>{!loading && data.civilStart + "-" + data.civilEnd}</code>
+        </pre>
       </div>
-
     </div>
   );
 }
